37b37675e448b57b1ae21082f05f50686ee45b2f
barbara-mooney <60058920+barbara-mooney@users.noreply.github.com>
2020-04-14 18:34:18 -0700

    Updated file

--- src/topspot.jsx
+++ src/topspot.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 export default props => (
-    <div>        
-        <div className = "card card-body">
+    <div>
+        <div className = "card card-body spot">
             <h3>{props.name}</h3>
             <p>{props.description}</p>
             <a href={props.href} className="btn btn-link">Go to Google Maps</a>
@@ -12,4 +12,4 @@ export default props => (
 
 
 // 2) WHY COULDN'T I MAKE CHANGES TO THE TOPSPOTS FILE... TO COMBINE THE LINK? JUST TO RENDER?
-// 3) HOW DO I STYLE THE WEBPAGE? I TRIED TO LINK A CSS STYLE SHEET AND WOULD GIVE ME ERRORS 
+// 3) HOW DO I STYLE THE WEBPAGE? I TRIED TO LINK A CSS STYLE SHEET AND WOULD GIVE ME ERRORS
